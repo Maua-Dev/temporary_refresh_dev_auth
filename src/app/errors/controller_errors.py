763b6a9a45d632d769508d3e85276cfb14b4c384
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-05-13 11:19:40 -0300

    added wrong type errors, missing errors, param not validated error

--- src/app/errors/controller_errors.py
+++ src/app/errors/controller_errors.py
@@ -1,5 +1,14 @@
 from .base_error import BaseError
 
-class MissingParameter(BaseError):
+class MissingParameters(BaseError):
     def __init__(self, param: str):
         super().__init__(f'Field {param} is required')
+        
+class WrongTypeParameters(BaseError):
+    def __init__(self, field_name: str, field_type_expected: str, field_type_received):
+        super().__init__(f'Field {field_name} isn\'t in the right type.\n Received: {field_type_expected}.\n Expected: {field_type_received}')
+        
+        
+class Forbidden(BaseError):
+    def __init__(self):
+        super().__init__(f'You don\'t have permission to access this resource')
--- src/app/main.py
+++ src/app/main.py
@@ -3,39 +3,42 @@ from mangum import Mangum
 import boto3
 import os
 from botocore.exceptions import ClientError
-
-
+from src.app.errors.controller_errors import WrongTypeParameters, MissingParameters
+from src.app.errors.entity_errors import ParamNotValidated
 
 app = FastAPI()
 
 @app.post("/refresh_token")
 def refresh_token(request: dict):
-    token = request.get("refresh_token")
-    stage = request.get("stage")
-
-    if not token:
-        raise HTTPException(status_code=400, detail="Missing token")
-    
-
-    if not stage:
-        raise HTTPException(status_code=400, detail="Missing stage")
-    
-    if stage not in ["dev", "prod"]:
-        raise HTTPException(status_code=400, detail="Invalid stage")
-    
-    if stage == "dev":
-        userpool_arn = os.getenv("AUTH_DEV_SYSTEM_USERPOOL_ARN_DEV")
-
-    elif stage == "prod":
-        userpool_arn = os.getenv("AUTH_DEV_SYSTEM_USERPOOL_ARN_PROD")
-
-
-    # get client id based on userpool arn
-    client = boto3.client("cognito-idp")
-    response = client.list_user_pool_clients(UserPoolId=userpool_arn.split("/")[1])
-    client_id = response["UserPoolClients"][0]["ClientId"]
-    
     try:
+        if request.get("refresh_token") is None:
+            raise MissingParameters("refresh_token")
+        if request.get("stage") is None:
+            raise MissingParameters("stage")
+        
+        if type(request.get("refresh_token")) != str:
+            raise WrongTypeParameters("refresh_token", "str", type(request.get("refresh_token")).__name__)
+        if type(request.get("stage")) != str:
+            raise WrongTypeParameters("stage", "str", type(request.get("stage")).__name__)
+        
+        token = request.get("refresh_token")
+        stage = request.get("stage")
+        
+        if stage not in ["dev", "prod"]:
+            raise ParamNotValidated("stage", "stage must be 'dev' or 'prod'")
+        
+        if stage == "dev":
+            userpool_arn = os.getenv("AUTH_DEV_SYSTEM_USERPOOL_ARN_DEV")
+
+        elif stage == "prod":
+            userpool_arn = os.getenv("AUTH_DEV_SYSTEM_USERPOOL_ARN_PROD")
+
+
+        # get client id based on userpool arn
+        client = boto3.client("cognito-idp")
+        response = client.list_user_pool_clients(UserPoolId=userpool_arn.split("/")[1])
+        client_id = response["UserPoolClients"][0]["ClientId"]
+    
         response = client.initiate_auth(
             ClientId=client_id,
             AuthFlow='REFRESH_TOKEN_AUTH',
@@ -54,7 +57,12 @@ def refresh_token(request: dict):
             raise HTTPException(status_code=401, detail="Invalid token")
         else:
             raise HTTPException(status_code=500, detail="Internal server error")
-
+    except MissingParameters as e:
+        raise HTTPException(status_code=400, detail=str(e.message))
+    except WrongTypeParameters as e:
+        raise HTTPException(status_code=400, detail=str(e.message))
+    except ParamNotValidated as e:
+        raise HTTPException(status_code=400, detail=str(e.message))
         
 
 handler = Mangum(app, lifespan="off")
